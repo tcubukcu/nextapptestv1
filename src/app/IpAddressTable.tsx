5f4ac215502143ed940b8e4aa44c70c09375d00d
Armonika Dijital <armonikadijital@gmail.com>
2025-03-22 14:27:28 +0300

    yk2

--- src/app/IpAddressTable.tsx
+++ src/app/IpAddressTable.tsx
@@ -12,9 +12,65 @@ async function getVercelIp() {
 }
 
 async function getykXML() {
-  const res = await axios.get<IpifyResponse>('https://testwebservices.yurticikargo.com/KOPSWebServices/ShippingOrderDispatcherServices?wsdl');
+  const res = await axios.get<IpifyResponse>('http://testwebservices.yurticikargo.com:9090/KOPSWebServices/ShippingOrderDispatcherServices?wsdl');
   return res;
 }
+const cargoUrl =
+  "http://webservices.yurticikargo.com:8080/KOPSWebServices/ShippingOrderDispatcherServices?wsdl";
+const cargoHeaders = { "Content-Type": "text/xml;charset=UTF-8" };
+const queryShipmentDetailBody = `
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ship="http://yurticikargo.com.tr/ShippingOrderDispatcherServices">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <ship:queryShipmentDetail>
+         <!--Optional:-->
+         <wsUserName>1075G1048073138</wsUserName>
+         <!--Optional:-->
+         <wsPassword>i8u6G15e5c618s5x</wsPassword>
+         <!--Optional:-->
+         <wsLanguage>?</wsLanguage>
+         <!--Zero or more repetitions:-->
+         <keys>LT6136424431849</keys>
+         <keyType>?</keyType>
+         <addHistoricalData>?</addHistoricalData>
+         <onlyTracking>?</onlyTracking>
+         <jsonData>?</jsonData>
+      </ship:queryShipmentDetail>
+   </soapenv:Body>
+</soapenv:Envelope>
+  `;
+
+async function queryShipmentDetail() {
+  const response = await fetch(cargoUrl, {
+    method: "POST", 
+    headers: {
+      ...cargoHeaders,
+    },
+    body: queryShipmentDetailBody,
+    // body: createShipmentBody,
+  });
+  return JSON.stringify(response.statusText);
+}
+
+async function queryShipmentDetailwithproxy(fixieUrl: URL) {
+  try {
+    const response = await axios.post(cargoUrl, queryShipmentDetailBody, {
+    headers: {
+      ...cargoHeaders,
+    },
+    proxy: {
+      protocol: 'http',
+      host: fixieUrl.hostname,
+      port: parseInt(fixieUrl.port, 10),
+      auth: { username: fixieUrl.username, password: fixieUrl.password }
+    }
+  });
+  return JSON.stringify(response.statusText);
+}
+catch (error) {
+  console.error("Error creating shipment:", error);
+  throw error; // Rethrow the error if needed  
+}}
 
 async function getFixieIp(fixieUrl: URL) {
   const res = await axios.get<IpifyResponse>('https://api.ipify.org?format=json', {
@@ -46,7 +102,11 @@ export default async function IpAddressTable() {
   const vercelIp = await getVercelIp();
   const fixieIp = await getFixieIp(fixieUrl);
   const ykXML = await getykXML();
-  console.log(ykXML);
+  // console.log(JSON.stringify(ykXML.data));
+  const shipment = await queryShipmentDetail();
+  const shipmentwithproxy = await queryShipmentDetailwithproxy(fixieUrl);
+  console.log("Fixie Giden Sorgu : ",shipmentwithproxy); 
+  console.log("Vercel Giden Sorgu : ",JSON.stringify(shipment));
   return (
     <table className={styles.table}>
       <tbody>
@@ -59,8 +119,12 @@ export default async function IpAddressTable() {
           <td>{fixieIp}</td>
         </tr>
         <tr>
-          <th>YK XML Response</th>
-          <td>{JSON.stringify(ykXML.data)}</td>
+          <th>YK XML Response - Vercel IP'si ile gönderilen POST</th>
+          <td>{JSON.stringify(shipment)}</td>
+        </tr>
+        <tr>
+          <th>YK XML Response - Fixie IP'si ile gönderilen POST</th>
+          <td>{JSON.stringify(shipmentwithproxy)}</td>
         </tr>
       </tbody>
     </table>
